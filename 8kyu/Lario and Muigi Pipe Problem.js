4d8045abab8e7482e75a83610c5467af2c3ff60a
Kaycee Nuel <heykayceee@gmail.com>
2023-01-11 04:31:58 +0100

    Lario and Muigi Pipe Problem.js

---
+++ 8kyu/Lario and Muigi Pipe Problem.js
@@ -0,0 +1,18 @@
+// Task
+// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+// Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+//MY SOLUTION
+
+function pipeFix(numbers){
+    var first = numbers[0];
+    var last = numbers[numbers.length-1];
+    
+    var arr = [];
+    for(var i = first; i <= last; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }

aea046cf5e1f28b8ec0160394688a3082521345c
Kaycee Nuel <heykayceee@gmail.com>
2023-01-12 12:04:36 +0100

    convert a binary number to a decimal

---
+++ 8kyu/Bin to Decimal.js
@@ -0,0 +1,11 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+//MY SOLUCTION
+
+function binToDec(bin){
+    return parseInt(bin ,2)
+  }
+
+    //   parseInt() is a JavaScript function that converts a string to an integer.
+    //   It takes two arguments: the first argument is the string to be converted, and 
+    //   the second argument (optional) is the radix (or base) of the number system to be used.

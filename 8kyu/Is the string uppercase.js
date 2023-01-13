33baa85bedba4c3a545775ec013138b5304ee775
Kaycee Nuel <heykayceee@gmail.com>
2023-01-13 11:07:06 +0100

    make it an Uppercase

---
+++ 8kyu/Is the string uppercase.js
@@ -0,0 +1,16 @@
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+//MY SOLUTION
+String.prototype.isUpperCase = function(s) {
+  
+    // your code here
+    return !this.match(/[a-z]/);
+  }

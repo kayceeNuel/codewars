8351828be1f3e7525cd459d55f23edb69aafe0d6
Kaycee Nuel <heykayceee@gmail.com>
2023-01-24 03:22:02 +0100

    concat and split

---
+++ 8kyu/ Methods of String object--concat() split() and its good friend join().js
@@ -0,0 +1,18 @@
+// Task
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+// For example:
+
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+//MY SOLUTION 
+function splitAndMerge(string, separator) {
+    const words = string.split(" ")
+    const merge = words.map( word => word.split("").join(separator));
+    return merge.join(" ")
+  }

6a39e714fbba9ee19f2464d919c374237a283d2b
Kaycee Nuel <heykayceee@gmail.com>
2023-01-26 15:18:48 +0100

    string reversal

---
+++ 7kyu/Simple string reversal.js
@@ -0,0 +1,21 @@
+// In this Kata, we are going to reverse a string while maintaining the spaces (if any) in their original place.
+
+// For example:
+
+// solve("our code") = "edo cruo"
+// -- Normal reversal without spaces is "edocruo". 
+// -- However, there is a space at index 3, so the string becomes "edo cruo"
+
+// solve("your code rocks") = "skco redo cruoy". 
+// solve("codewars") = "srawedoc"
+
+function solve(str)
+{
+  var s = "";
+  for (let i = str.length - 1; i >= 0; i--)
+  {
+    if (str[i] != " ")        s += str[i];
+    if (str[s.length] == " ") s += str[s.length];
+  }
+  return s;
+}

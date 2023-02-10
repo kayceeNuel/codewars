451aa422887e3099ae7ce2996bdaa4b321b5370e
Kaycee Nuel <heykayceee@gmail.com>
2023-02-10 23:35:41 +0100

    push element into an enter array

---
+++ 8kyu/Push a hash/an object into array.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+
+//SOLUTION
+
+
+// Declare an array named "items"
+// push a new object with properties "a" and "c" to the "items" array using the push array method
+// return the "items" array
+
+let items = []
+items.push ({a: "b", c: "d"})

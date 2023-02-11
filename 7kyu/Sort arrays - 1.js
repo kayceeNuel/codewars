4570fbd2d53b2ddc227c4d5a89d53548cde30d99
Kaycee Nuel <heykayceee@gmail.com>
2023-02-11 23:01:38 +0100

    rearranged list in a specific order

---
+++ 7kyu/Sort arrays - 1.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// input: names - unsorted array
+// output: sorted array
+
+//PESUDOCODE
+// function sortme (name) {
+//   return the list of sorted arrays using the sort method in a lexicographical order
+//   The sort method takes a list as input and returns the same list, but with 
+//     the elements rearranged in the specified order.
+// }
+
+
+sortme = function( names ){
+  return names.sort()
+}

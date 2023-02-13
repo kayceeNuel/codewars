ce935e1f9f76aaa1792df42c50691f2633ec7a21
Kaycee Nuel <heykayceee@gmail.com>
2023-02-13 15:34:50 +0100

    add up array

---
+++ 6kyu/+1 Array.js
@@ -0,0 +1,45 @@
+// Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+// the array can't be empty
+// only non-negative, single digit integers are allowed
+// Return nil (or your language's equivalent) for invalid inputs.
+
+// Examples
+// Valid arrays
+
+// [4, 3, 2, 5] would return [4, 3, 2, 6]
+// [1, 2, 3, 9] would return [1, 2, 4, 0]
+// [9, 9, 9, 9] would return [1, 0, 0, 0, 0]
+// [0, 1, 3, 7] would return [0, 1, 3, 8]
+
+
+// SOLUTION
+function upArray(arr){
+    if (!Array.isArray(arr) || arr.length === 0) {
+     return null;
+   }
+ 
+   for (const item of arr) {
+     if (typeof item !== 'number' || item < 0 || item > 9) {
+       return null;
+     }
+   }
+ 
+   let carry = 1;
+   for (let i = arr.length - 1; i >= 0; i--) {
+     arr[i] += carry;
+     carry = 0;
+     if (arr[i] > 9) {
+       arr[i] = 0;
+       carry = 1;
+     } else {
+       break;
+     }
+   }
+ 
+   if (carry === 1) {
+     arr.unshift(1);
+   }
+ 
+   return arr;
+ }

64c8faefcb32509ce8523b95c515f326f68fb4b6
Kaycee Nuel <heykayceee@gmail.com>
2023-02-16 23:11:26 +0100

    produce array

--- 7kyu/Possibilities Array.js
+++ 7kyu/Possibilities Array.js
@@ -1,3 +1,5 @@
+//DESCRIPTION
+
 // A non-empty array a of length n is called an array of all possibilities if it contains all numbers between [0,a.length-1].Write a method named isAllPossibilities that accepts an integer array and returns true if the array is an array of all possibilities, else false.
 
 // Example:
---
+++ 7kyu/Product Array.js
@@ -0,0 +1,18 @@
+// Task
+// Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+
+// Notes
+// Array/list size is at least 2 .
+
+// Array/list's numbers Will be only Positives
+
+// Repetition of numbers in the array/list could occur.
+
+// Input >> Output Examples
+// productArray ({12,20}) ==>  return {20,12}
+
+//SOLUTION
+function productArray(numbers){
+    return numbers.map(x => numbers.reduce( (a,b) => a*b) /x)
+  }
+  

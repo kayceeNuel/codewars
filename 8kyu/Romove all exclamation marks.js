d7586d6379a4ead0951e12416aa2c8c6dd1e4b75
Kaycee Nuel <heykayceee@gmail.com>
2023-02-23 20:17:32 +0100

    remove exclamation marks from the strings

---
+++ 8kyu/Romove all exclamation marks.js
@@ -0,0 +1,18 @@
+// Description:
+// Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi!"
+// remove("Hi!!!") === "Hi!"
+// remove("!Hi") === "Hi!"
+// remove("!Hi!") === "Hi!"
+// remove("Hi! Hi!") === "Hi Hi!"
+// remove("Hi") === "Hi!"
+
+//SOLUTION
+
+function remove(string) {
+    return string.slice(-1) + string.slice(1, -1) + string.slice(0, 1);
+
+  }
+  

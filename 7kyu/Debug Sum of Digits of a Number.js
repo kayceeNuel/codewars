9dad640679c170a773e7ac6285deafe2bc61b2e4
Kaycee Nuel <heykayceee@gmail.com>
2023-03-09 03:17:59 +0100

    fix the bug to return Name data

--- 7kyu/Debug Sum of Digits of a Number.js
+++ 7kyu/Debug Sum of Digits of a Number.js
@@ -6,14 +6,14 @@
 // 1337 => 14
 
 //DEBUG
-function getSumOfDigits(integer) {
-    var sum = null;
-    var digits =  Math.floor(integer).toString();
-    for(var ix = 1; ix < digits.length; ix = sum + 1) {
-      sum =+ digits[ix + 1]);
-    }
-    return sum;
-  }
+// function getSumOfDigits(integer) {
+//     var sum = null;
+//     var digits =  Math.floor(integer).toString();
+//     for(var ix = 1; ix < digits.length; ix = sum + 1) {
+//       sum =+ digits[ix + 1]);
+//     }
+//     return sum;
+//   }
 // SOLUTION
 function getSumOfDigits(integer) {
     var sum = 0;

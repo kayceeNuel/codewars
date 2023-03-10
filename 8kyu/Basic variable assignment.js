821948fb61e5b6433c7897320f4ddec7a55690f2
Kaycee Nuel <heykayceee@gmail.com>
2023-03-10 04:43:05 +0100

    find the bug in the variable

---
+++ 8kyu/Basic variable assignment.js
@@ -0,0 +1,13 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+//BUG 
+// var a == "code";
+// var b == "wa.rs";
+// var name == a + b;
+
+//find the error in the variables 
+
+//SOLUTION
+var a = "code";
+var b = "wa.rs";
+var name = a + b;

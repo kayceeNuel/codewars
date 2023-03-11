d848642717a1ec90a5374b93a36ad885dc7ee36a
Kaycee Nuel <heykayceee@gmail.com>
2023-03-11 04:05:51 +0100

    fix the bug

---
+++ 6kyu/Closures and Scopes.js
@@ -0,0 +1,35 @@
+// We want to create a function, which returns an array of functions, which return their index in the array. For better understanding, here an example:
+
+// var callbacks = createFunctions(5); // create an array, containing 5 functions
+
+// callbacks[0](); // must return 0
+// callbacks[3](); // must return 3
+// We already implemented that function, but when we actually run the code, the result doesn't look like what we expected. Can you spot, what's wrong with it? A test fixture is also available
+
+
+//BUG
+function createFunctions(n) {
+    var callbacks = [];
+  
+    for (var i=0; i<n; i++) {
+      callbacks.push(function() {
+        return i;
+      });
+    }
+    
+    return callbacks;
+  }
+
+  //SOLUTION
+  function createFunctions(n) {
+    var callbacks = [];
+  
+    for (var i = 0; i < n; i++) {
+      let index = i; // create a new variable scoped to the function
+      callbacks.push(function() {
+        return index; // use the new variable instead of i
+      });
+    }
+    
+    return callbacks;
+  }

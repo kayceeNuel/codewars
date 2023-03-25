99169d704084fd3373acffe95ca06d5c34ee93f5
Kaycee Nuel <heykayceee@gmail.com>
2023-03-25 21:57:02 +0100

    make the code work

---
+++ 7kyu/JavaScript seems to be broken.js
@@ -0,0 +1,17 @@
+/**
+ * There is some horribly rotten script that is not required in your project anymore, and it is affecting the way your code is supposed to work.
+
+For some reason you don't have an access to that rotten piece of code but you want to get your code working as expected in any possible way.
+
+Go and get it working!
+ */
+
+//bug 
+function semicolonSeparationToCommaSeparation(input) {
+    return input.split(";").join(",");  
+  }
+
+  //solution
+  function semicolonSeparationToCommaSeparation(input) {
+    return input.split(";")._join(",");  
+  }

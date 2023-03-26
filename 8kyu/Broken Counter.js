7eabdd4533a9f87d350fb59862afeb6a68769d8b
Kaycee Nuel <heykayceee@gmail.com>
2023-03-26 23:29:43 +0100

    respond to the number of the dogs

---
+++ 8kyu/Broken Counter.js
@@ -0,0 +1,37 @@
+/**
+ * Our counter prototype is broken. Can you spot, what's wrong here?
+
+ */                                                                                      
+// BUG
+// function Counter() {
+//     this.value = 0;
+//   }
+  
+//   Counter.prototype.increase() {
+//     this.value++;
+//   };
+  
+//   Counter.prototype.getValue() {
+//     return this.value;
+//   };
+  
+//   Counter.prototype.reset() {
+//     this.value = 0;
+//   };
+
+  // SOLUTIONd                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+  function Counter() {
+    this.value = 0;
+  }
+  
+  Counter.prototype.increase = function (){
+    return this.value++;
+  };
+  
+  Counter.prototype.getValue = function () {
+    return this.value;
+  };
+  
+  Counter.prototype.reset = function (){
+   return this.value = 0;
+  };

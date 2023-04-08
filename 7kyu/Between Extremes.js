ef5b408bbe3b8c118ccb1aa8f82f2be319339f98
Kaycee Nuel <heykayceee@gmail.com>
2023-04-08 10:54:48 +0100

    get the difference btw the largest and smallest values

---
+++ 7kyu/Between Extremes.js
@@ -0,0 +1,21 @@
+/*Given an array of numbers, return the difference between the largest and smallest values.
+
+For example:
+
+[23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+[1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow. */
+
+//solution
+
+function betweenExtremes(numbers) {
+    //get the largest number
+      const largest_number = Math.max(...numbers);
+    //Get the smallest number 
+    const smallest_number = Math.min(...numbers);
+    //return the difference btw the largest and smallest values
+    return largest_number - smallest_number;
+    
+  }

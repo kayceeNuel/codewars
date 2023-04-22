0b019141ed2282594f11ac192ef16afe77d15645
Kaycee Nuel <heykayceee@gmail.com>
2023-04-23 00:41:00 +0100

    fix the Pascal Problem

---
+++ 6kyu/Pascal Problem.js
@@ -0,0 +1,30 @@
+/** Here is a classic, Pascal's triangle.
+
+The pascal function should return a nested array, such as in the example below,
+
+pascal(5) // should return [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+However, when running the given code, the result is an empty array.
+
+Your job? Fix it! */
+
+//BUG 
+function pascal(depth) {
+    var results = [];
+    var temp = [];
+    for (var r=0; r < depth; r++) {
+      for (var c=0; c < r; c++) {
+        temp.push(calculate(c, r));
+      }
+      results.push(temp);
+      temp = [];
+    }
+    return results;
+  }
+  
+  function calculate(c, r) {
+    if ( c == r || c == 1 )
+      return 1;
+    else
+      return calculate(c-1, r-1) + calculate(c, r-1);
+  }
